6d4686f97dc59b04f866597928f2e0b3f177dc71
MathieuMorel62 <mormth@aol.com>
2022-12-12 14:04:38 -0800

    add line for second

--- swap.c
+++ swap.c
@@ -19,9 +19,12 @@ void swap(stack_t **stack, unsigned int line_number)
 		fprintf(stderr, "L%d: can't swap, stack too short\n", line_number);
 		exit(EXIT_FAILURE);
 	}
+	second = (*stack)->next;
+
 	top->prev = second;
 	second->prev = NULL;
 	top->next = second->next;
 	second->next = top;
+
 	*stack = second;
 }
